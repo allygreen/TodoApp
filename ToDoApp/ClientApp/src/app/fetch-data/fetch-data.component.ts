5a4ae214a085a19b9ebeba115550e281ce249324
Alasdair Green <alasdair@admgsolutions.com>
2023-09-30 23:48:03 +0100

    Inline editing of todo notes

--- ToDoApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ToDoApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -9,6 +9,7 @@ export class FetchDataComponent {
   public showForm = false;
   public todo: ToDoList[] = [];
   public newTodo: Partial<ToDoList> = {};
+  public editingField: string | null = null; // Track which field is being edited
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     this.fetchTodos();
@@ -33,13 +34,41 @@ export class FetchDataComponent {
       error => console.error(error)
     );
   }
+
+  // Function to handle inline editing
+  editCell(field: string, todoItem: ToDoList): void {
+    this.editingField = field;
+  }
+
+  // Function to save changes made to a cell
+  saveCell(todoItem: ToDoList): void {
+    // Send a PUT request to update the backend with the changes
+    this.http.put<ToDoList>(this.baseUrl + 'todo/' + todoItem.id, todoItem).subscribe(
+      updatedTodo => {
+        // Update the item in the todo list with the updated data from the server
+        const index = this.todo.findIndex(item => item.id === updatedTodo.id);
+        if (index !== -1) {
+          this.todo[index] = updatedTodo;
+        }
+        this.editingField = null; // Exit edit mode
+      },
+      error => console.error(error)
+    );
+  }
+
+  // Function to cancel editing and exit edit mode
+  cancelEdit(): void {
+    this.editingField = null; // Exit edit mode
+  }
 }
 
-interface ToDoList {
+export interface ToDoList {
+  id: number;
   createdDate: string;
   completed: boolean;
   title: string;
   summary: string;
   description: string;
   dateCompleted: string;
+  editingField?: string;
 }
